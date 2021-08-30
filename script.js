56707dd1ccae6bdc281adcb7c24f9484afd87f5b
Minhjkl <87932877+Minhjkl@users.noreply.github.com>
2021-08-30 22:47:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".navbar").style.backgroundColor="transparent"
+window.addEventListener("scroll",function(){
+    if(window.scrollY===0){
+        document.querySelector(".navbar").style.backgroundColor="transparent"
+    }
+})
